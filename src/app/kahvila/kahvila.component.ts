6badbb0a90648d72a8bbe8ddadb5224d6dc96141
Auli Färm <s2000578@edu.bc.fi>
2021-03-15 16:01:38 +0200

    first commit

---
+++ src/app/kahvila/kahvila.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-kahvila',
+  templateUrl: './kahvila.component.html',
+  styleUrls: ['./kahvila.component.css']
+})
+export class KahvilaComponent implements OnInit {
+  @Output() myyntiTapahtuma = new EventEmitter<{poydanNumero:string, myyntiMaara: string}>();
+  @Output() tarjoiluTapahtuma= new EventEmitter<{poydanNumero:string, myyntiMaara: string}>();
+  uusipoydanNumero = '';
+  uusimyyntiMaara = '';
+
+  kahviaMyyty() {
+    this.myyntiTapahtuma.emit({
+      poydanNumero: this.uusipoydanNumero,
+      myyntiMaara: this.uusimyyntiMaara
+    })
+  }
+
+  kahviaTarjoiltu() {
+    this.tarjoiluTapahtuma.emit({
+      poydanNumero: this.uusipoydanNumero,
+      myyntiMaara: this.uusimyyntiMaara
+    })
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
